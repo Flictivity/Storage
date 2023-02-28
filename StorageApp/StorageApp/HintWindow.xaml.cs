5eae700a7a47d56aee3806aac547372f142a83b7
Rinat050 <safiullin.rinat.05@gmail.com>
2023-02-28 22:14:01 +0300

    add Hint Window

---
+++ StorageApp/StorageApp/HintWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace StorageApp
+{
+    /// <summary>
+    /// Interaction logic for HintWondow.xaml
+    /// </summary>
+    public partial class HintWondow : Window
+    {
+        private string HintText = "";
+
+        public HintWondow(string hintText, CornerRadius borderCornerRadius)
+        {
+            InitializeComponent();
+
+            mainBorder.CornerRadius = borderCornerRadius;
+
+            this.WindowStartupLocation = WindowStartupLocation.Manual;
+            this.Left = 800;
+            this.Top = 400;
+
+            HintText = hintText;
+            Binding binding = new Binding();
+            binding.Source = HintText;
+            tbHint.SetBinding(TextBlock.TextProperty, binding);
+        }
+    }
+}
