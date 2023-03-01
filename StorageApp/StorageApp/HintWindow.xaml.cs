3050229d4254c6263f2daacd529c8a9570338d7a
Rinat050 <safiullin.rinat.05@gmail.com>
2023-03-01 17:15:25 +0300

    add hints for userTasksPage

--- StorageApp/StorageApp/HintWindow.xaml.cs
+++ StorageApp/StorageApp/HintWindow.xaml.cs
@@ -20,21 +20,29 @@ namespace StorageApp
     public partial class HintWondow : Window
     {
         private string HintText = "";
+        public delegate void OnClosedDelegate();
+        public event OnClosedDelegate OnClosedEvent;
 
-        public HintWondow(string hintText, CornerRadius borderCornerRadius)
+        public HintWondow(string hintText, CornerRadius borderCornerRadius, double left, double top)
         {
             InitializeComponent();
 
             mainBorder.CornerRadius = borderCornerRadius;
 
             this.WindowStartupLocation = WindowStartupLocation.Manual;
-            this.Left = 800;
-            this.Top = 400;
+            this.Left = left;
+            this.Top = top;
 
             HintText = hintText;
             Binding binding = new Binding();
             binding.Source = HintText;
             tbHint.SetBinding(TextBlock.TextProperty, binding);
+        }
+
+        private void EventClose(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+            OnClosedEvent?.Invoke();
         }
     }
 }
